433443b8a1babab136a43493431824fc0c2cf559
ivayloAngelov92 <i.r.angelov92@gmail.com>
2023-12-10 04:46:22 +0000

    update

--- client/src/services/drinkService.js
+++ client/src/services/drinkService.js
@@ -15,19 +15,9 @@ export const getOne = async (gameId) => {
 }
 
 export const getLatest = async () => {
-    // const query = new URLSearchParams({
-    //     sortBy: `_createdOn desc`,
-    //     offset: 0,
-    //     pageSize: 3,
-    // });
-
-    // const query = encodeURIComponent(`offset=0&pageSize=3`);
-    
     const result = await request.get(`${baseUrl}?sortBy=_createdOn%20desc&offset=0&pageSize=3`);
-        
-    console.log(result);
-
-    return result;
+    return result
+    
 }
 
 export const create = async (gameData) => {
